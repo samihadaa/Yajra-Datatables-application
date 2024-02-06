c7223b3fb0ddce6aa8f9a1383e972c37d36b1ece
samihadaa <samihada.madaraa@gmail.com>
2024-02-06 11:57:49 +0100

    initial commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index(){
+
+    }
+    public function show(){
+
+    }
+    public function store(){
+
+    }
+    public function update(){
+
+    }
+    public function destroy(){
+
+    }
+}
