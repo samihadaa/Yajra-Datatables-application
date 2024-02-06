6394ad873d91dbaf0b10349381d1ff8a59fef5ba
samihadaa <samihada.madaraa@gmail.com>
2024-02-06 15:51:40 +0100

    Feat : adding CRUD functionnalities to todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,23 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 
 class TodoController extends Controller
 {
     public function index(){
-
+        $todos = Todo::orderBy('created_at', 'DESC')->get();
+        return response()->json([
+            'todos' => $todos,
+            'status' => 200,
+        ]);
     }
-    public function show(){
-
+    public function show(Todo $todo){
+        return response()->json([
+            'todo' => $todo,
+            'status' => 200,
+        ]);
     }
-    public function store(){
-
+    public function store(TodoRequest $request)
+    {
+        $todoData = $request->validated();
+        $todo = Todo::create($todoData);
+        return response()->json([
+        'todo' => $todo,
+        'status' => 201,
+        ]);
     }
-    public function update(){
-
+    public function update(Todo $todo, TodoRequest $request){
+        $todo->update($request->validated());
+        return response()->json([
+            'todo' => $todo,
+            'status' => 200,
+            ]);
     }
-    public function destroy(){
-
+    public function destroy(Todo $todo){
+        $todo->delete();
+        return response()->json([
+            'message' => 'todo deleted successfully',
+            'status' => 200,
+            ]);
     }
 }
