c3c5d07f2808c03da500357049e0f88485d40be9
samihadaa <samihada.madaraa@gmail.com>
2024-02-07 11:56:45 +0100

    Feat : Installing Vue and bootstrap in my laravel project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import {createApp} from 'vue'
+import app from './app.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.min.js'
+createApp(app).use(bootstrap).mount("#app")
